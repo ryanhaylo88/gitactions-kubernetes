02805e207d178b2e5f0b7df7b47e0d569125da18
Ryan <your-email@example.com>
2026-03-17 17:08:57 +0000

    Change main.py api message to test redploy via push

--- app/main.py
+++ app/main.py
@@ -6,4 +6,4 @@ VERSION = os.environ.get("APP_VERSION", "v1")
 
 @app.get("/")
 def read_root():
-    return {"message": f"Hello from Kubernetes 🚀 - {VERSION}"}
+    return {"message": f"Hello from Kubernetes 2.0🚀 - {VERSION}"}
